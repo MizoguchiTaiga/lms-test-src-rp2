ffe65a36a8f18b0cd5fb5579b3116747e68df9f5
t-mizoguchi <t.mizoguchi.aivan@gmail.com>
2025-11-25 16:42:35 +0900

    Case07.java レポート登録画面で日報を提出するテストコードの追加
    レポートが未提出の研修日の日報を提出し、確認用ボタンが更新されることを確認するテストコードを作成した。
    期待値は以下の通り
    報告内容：今日はよくできた。
    確認ボタンの更新
    更新前：日報【デモ】を提出する
    更新後：提出済み日報【デモ】を確認する

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f03_report/Case07.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f03_report/Case07.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f03_report;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.Assert.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,10 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import jp.co.sss.lms.ct.util.WebDriverUtils;
 
 /**
  * 結合テスト レポート機能
@@ -35,35 +40,98 @@ public class Case07 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		// ログイン画面に遷移する
+		goTo("http://localhost:8080/lms");
+
+		// 画面遷移が正しく行われたか確認する
+		WebElement title = WebDriverUtils.webDriver.findElement(By.tagName("h2"));
+		assertEquals("ログイン", title.getText());
+
+		// エビデンスを取得する
+		getEvidence(new Object(){});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
-	}
+		// ログインIDとパスワードを入力する
+		WebDriverUtils.webDriver.findElement(By.id("loginId")).sendKeys("StudentAA01");
+		WebDriverUtils.webDriver.findElement(By.id("password")).sendKeys("TestAA01");
+
+		// エビデンスを取得する①
+		getEvidence(new Object(){}, "01");
+
+		// ログインボタンを押下する
+		WebDriverUtils.webDriver.findElement(By.className("btn-primary")).click();
+
+		//ログインに成功し、画面遷移が正しく行われたか確認する
+		WebDriverUtils.visibilityTimeout(By.className("active"), 10);
+		WebElement title = WebDriverUtils.webDriver.findElement(By.className("active"));
+		assertEquals("コース詳細", title.getText());
+
+		// エビデンスを取得する②
+		getEvidence(new Object(){}, "02");	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 未提出の研修日の「詳細」ボタンを押下しセクション詳細画面に遷移")
 	void test03() {
-		// TODO ここに追加
+		// 未提出の研修日の「詳細」ボタンを押下する
+		WebDriverUtils.webDriver.findElement(By.xpath("//*[@id='main']/div/div[3]/div[2]/table/tbody/tr[1]/td[5]/form/input[3]")).click();
+		
+		// 画面遷移が正しく行われたか確認する
+		WebDriverUtils.visibilityTimeout(By.tagName("h3"), 10);
+		WebElement title = WebDriverUtils.webDriver.findElement(By.tagName("h3"));
+		assertEquals("本日の内容", title.getText());
+		
+		// エビデンスを取得する
+		getEvidence(new Object(){});
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「提出する」ボタンを押下しレポート登録画面に遷移")
 	void test04() {
-		// TODO ここに追加
+		// 「日報【デモ】を提出する」ボタンを押下する
+		WebDriverUtils.webDriver.findElement(By.xpath("//*[@id='sectionDetail']/table/tbody/tr[2]/td/form/input[5]")).click();
+		
+		// 画面遷移が正しく行われたか確認する
+		WebDriverUtils.visibilityTimeout(By.className("control-label"), 10);
+		WebElement label = WebDriverUtils.webDriver.findElement(By.className("control-label"));
+		assertEquals("本日の報告内容をお書きください。", label.getText());
+		
+		// エビデンスを取得する
+		getEvidence(new Object(){});
 	}
 
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 報告内容を入力して「提出する」ボタンを押下し確認ボタン名が更新される")
 	void test05() {
-		// TODO ここに追加
+		// 報告内容を入力する
+		WebElement report = WebDriverUtils.webDriver.findElement(By.id("content_0"));
+		report.clear();
+		report.sendKeys("今日はよくできた。");
+		
+		// エビデンスを取得する①
+		getEvidence(new Object(){}, "01");
+
+		// 「提出する」ボタンを押下する
+		WebDriverUtils.webDriver.findElement(By.className("btn-primary")).click();
+		
+		// 画面遷移が正しく行われたか確認する
+		WebDriverUtils.visibilityTimeout(By.tagName("h3"), 10);
+		WebElement title = WebDriverUtils.webDriver.findElement(By.tagName("h3"));
+		assertEquals("本日の内容", title.getText());
+		
+		// ボタン名が更新されていることを確認する
+		WebDriverUtils.visibilityTimeout(By.xpath("//*[@id='sectionDetail']/table/tbody/tr[2]/td/form/input[6]"), 10);
+		WebElement btn = WebDriverUtils.webDriver.findElement(By.xpath("//*[@id='sectionDetail']/table/tbody/tr[2]/td/form/input[6]"));
+		assertEquals("提出済み日報【デモ】を確認する", btn.getAttribute("value"));
+		
+		// エビデンスを取得する②
+		getEvidence(new Object(){}, "02");
 	}
 
 }
